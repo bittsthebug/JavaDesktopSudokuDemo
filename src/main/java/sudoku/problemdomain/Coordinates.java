12da192f6875232fa4ed236060080efd32407043
Vaishaliyadavv <vaishali.yadavv09@gmail.com>
2024-02-12 21:07:10 +0530

    Added Interface Made two @Override methods in Coordinates.java

--- src/main/java/sudoku/problemdomain/Coordinates.java
+++ src/main/java/sudoku/problemdomain/Coordinates.java
@@ -1,5 +1,7 @@
 package sudoku.problemdomain;
 
+import java.util.Objects;
+
 public class Coordinates {
     private final int x;
     private final int y;
@@ -15,5 +17,18 @@ public class Coordinates {
 
     public int getX() {
         return x;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
--- src/main/java/sudoku/problemdomain/SudokuGame.java
+++ src/main/java/sudoku/problemdomain/SudokuGame.java
@@ -3,9 +3,9 @@ package sudoku.problemdomain;
 import java.io.Serializable;
 
 public class SudokuGame implements Serializable {
+    public static final int GRID_BOUNDARY = 9;
     private final GameState gamestate;
     private final int[][] gridState;
-    public static final int GRID_BOUNDARY = 9;
 
     public SudokuGame(GameState gamestate, int[][] gridState) {
         this.gamestate = gamestate;
