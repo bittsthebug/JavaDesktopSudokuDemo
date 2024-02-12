a477f868dccb5b2a3c38beea358c48b20250b14b
Vaishaliyadavv <vaishali.yadavv09@gmail.com>
2024-02-12 20:40:30 +0530

    Made constructors and getter methods in SudokuGame.java and also in a new class called Coordinates.java

--- src/main/java/sudoku/problemdomain/SudokuGame.java
+++ src/main/java/sudoku/problemdomain/SudokuGame.java
@@ -1,4 +1,23 @@
 package sudoku.problemdomain;
 
-public class SudokuGame {
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+    private final GameState gamestate;
+    private final int[][] gridState;
+    public static final int GRID_BOUNDARY = 9;
+
+    public SudokuGame(GameState gamestate, int[][] gridState) {
+        this.gamestate = gamestate;
+        this.gridState = gridState;
+    }
+
+    public GameState getGamestate() {
+        return gamestate;
+    }
+
+    public int[][] getCopyOfGridState() {
+        return SudokuUtilities.copyToNewArray(gridState);
+    }
+
 }
